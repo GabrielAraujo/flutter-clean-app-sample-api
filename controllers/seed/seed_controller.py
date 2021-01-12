c8cda66315df7fe491b02de8b73b934cf2388149
Gabriel Araujo <gabriel.l.arauj@gmail.com>
2021-01-12 17:04:46 -0300

    Small space adjustment

--- controllers/seed/seed_controller.py
+++ controllers/seed/seed_controller.py
@@ -13,9 +13,9 @@ IS_OFFLINE = os.environ.get('IS_OFFLINE')
 
 if IS_OFFLINE:
   client = boto3.client(
-      'dynamodb',
-      region_name='localhost',
-      endpoint_url='http://localhost:8000'
+    'dynamodb',
+    region_name='localhost',
+    endpoint_url='http://localhost:8000'
   )
 else:
   client = boto3.client('dynamodb')
